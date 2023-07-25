759a3e39cace2cf5045b74aaaad0028cd8516580
Pratik <srivastavapratik35@gmail.com>
2023-07-25 22:25:36 +0530

    🗝️keyword added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ export const metadata = {
   title: "Pratik Srivastava - Portfolio",
   description:
     "Welcome to Pratik Srivastava's Portfolio - Showcasing a creative journey through design, art, and innovation. Explore a diverse collection of projects and achievements that reflect my passion and dedication. Get inspired by the power of imagination and the art of storytelling.",
+  keyword:
+    "Pratik Srivastava, Pratik, Srivastava, Portfolio, Pratik Srivastava Portfolio, Pratik Portfolio, Pratik Srivastava, Bit Sindri, Dhanbad, Jharia, India, Jharkhand, Jharkhand, India, BIT SINDRI, Jharia, BIT SINDRI, India",
 };
 
 export default function RootLayout({
